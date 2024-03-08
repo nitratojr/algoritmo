2cb2ff90584c8821184d19b9a34d50bf76e03f56
nitratojr <162368996+nitratojr@users.noreply.github.com>
2024-03-08 17:11:15 -0300

    Add files via upload

---
+++ conversao.js
@@ -0,0 +1,11 @@
+const idadeNumero = 18
+const idadeTexto = idadeNumero.tostring()
+console.log(typeof idadeNumero)
+console.log(typeof idadeTexto)
+const idadeTexto5 = '20'
+const idadeNumero5 = Number(idadeTexto5)
+console.log(typeof idadeTexto5)
+console.log(typeof idadeNumero5)
+const numero1 = '5'
+const numero2 = '9'
+console.log(Number(numero1)+Number(numero2))
---
+++ exercicio.js
@@ -0,0 +1,13 @@
+const meuNome = 'cleber'
+const sobrenome = 'junior'
+const idade = 17
+const estudante = true
+console.log('meu nome é',meuNome,'e meu sobrenome é',sobrenome,'e minha idade é',idade,'anos','e sou estudane?',estudante,".")
+const serie = 'mentalista'
+console.log(typeof serie)
+const temporada = 7
+console.log('minha serie favorita é',serie,'e ela tem',temporada,'temporadas',typeof serie,'e',typeof temporada)
+
+
+
+
