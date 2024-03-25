4a5494b72959d6b2e06b17e6f1840d385ea0f1b7
nitratojr <162368996+nitratojr@users.noreply.github.com>
2024-03-25 15:14:19 -0300

    Add files via upload

---
+++ main (1).js
@@ -0,0 +1,16 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+print('Hello World');
+
+/*var idade = 10;
+var status = idade >= 18?'adulto':'menor';
+print(status);*/
+ var palavra1 = 'alfa';
+ var palavra2 = 'beto';
+ var resultado = palavra1+palavra2
+ print(resultado)
---
+++ main.js
@@ -0,0 +1,62 @@
+/******************************************************************************
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+/*print('Hello World');
+var n1 = 5;
+var n2 = 3;
+var resultado;
+
+resultado = n1 == n2;
+print(n1,'-',n2,'?',resultado);
+resultado = n1 != n2;
+print(n1,'!=',n2,'?',resultado);
+resultado = n1 > n2;
+print(n1,'>',n2,'?',resultado);
+resultado = n1 < n2 ;
+print(n1,'<',n2,'?',resultado);
+resultado = n1 >= n2;
+print(n1,'>=',n2,'?',resultado);
+resultado = n1 <= n2;
+print(n1,'<=',n2,'?',resultado);*/
+
+/*var a1 = true && true;
+print(a1);
+var a2 = true && false;
+print(a2);
+var a3 = false && true;
+print(a3);
+var a4 = false && 3 == 4;
+print(a4);
+var a5 = 'gato' && 'cao';
+print(a5);
+var a6 = false && 'gato';
+print(a6);
+var a7 = 'gato' && false;
+print(a7);*/
+
+var a1 = true || true;
+print(a1);
+var a2 = true || false;
+print(a2);
+var a3 = false || true;
+print(a3);
+var a4 = false || 3 == 4;
+print(a4);
+var a5 = 'gato' || 'cao';
+print(a5);
+var a6 = false || 'gato';
+print(a6);
+var a7 = 'gato' || false;
+print(a7);
+
+
+
+
+
+
+
+
