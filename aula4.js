d75fb3334dd287d233b37a87ccbca4eda72f6f9a
nitratojr <162368996+nitratojr@users.noreply.github.com>
2024-04-01 17:21:26 -0300

    Add files via upload

---
+++ aula4.js
@@ -0,0 +1,35 @@
+/*print('Hello World');
+var n1 = 5;
+var n2 = 3;
+var resultado;
+
+resultado = n1 == n2;
+print(n1,'-',n2,'?',resultado);
+resultado = n1 != n2;
+print(n1,'!=',n2,'?',resultado);
+resultado = n1 > n2;
+print(n1,'>',n2,'?',resultado);
+resultado = n1 < n2 ;
+print(n1,'<',n2,'?',resultado);
+resultado = n1 >= n2;
+print(n1,'>=',n2,'?',resultado);
+resultado = n1 <= n2;
+print(n1,'<=',n2,'?',resultado);*/
+
+var a1 = true && true;
+print(a1);
+var a2 = true && false;
+print(a2);
+var a3 = false && true;
+print(a3);
+var a4 = false && 3 == 4;
+print(a4);
+var a5 = 'gato' && 'cao';
+print(a5);
+var a6 = false && 'gato';
+print(a6);
+var a7 = 'gato' && false;
+print(a7);
+
+
+
---
+++ aula5.js
@@ -0,0 +1,38 @@
+/*
+//cleber júnior 
+01/04/2024
+ Essas variaveis foram usadas para multplicar.*/
+
+/*let n1 = 20
+let n2 = 900
+const mult = 20 * 900
+console.log('seu resultado é',mult)*/
+
+/*// cleber júnior 
+01/04/2024
+Utilizei variaveis com numeros com a finalidade de dividi-los.*/
+let n1 = 20
+let n2 = 40
+const div = 20 / 40
+console.log('seu resultado é',div)
+/*// cleber júnior
+ 01/04/2024
+Essas varialveis tem a finalidade de somar os numeros.*/ 
+
+let n3 = 50
+let n4 = 50
+const soma = 50 + 50
+console.log('seu resultado é',soma)
+
+/*// cleber júnior
+01/04/2024
+variaveis com a finalidade de descobrir a porcentagem.*/
+let p = 0.5
+let q = 200
+const mult = 0.5 * 200
+console.log('porcentagem',mult)
+
+let n6 = 50
+let n5 = 50
+const sub = 50 - 50
+console.log('seu resultado é',sub)
