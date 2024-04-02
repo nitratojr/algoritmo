928690ea869ff89efa9ea45ad4e28433248f4b06
nitratojr <162368996+nitratojr@users.noreply.github.com>
2024-04-02 17:29:54 -0300

    Add files via upload

---
+++ exercicio1.js
@@ -0,0 +1,41 @@
+/*const nome = 'cleber'
+const id = 16
+
+console.log(`meu nome é ${nome}  e tenho  ${id}  anos`)//template string
+
+console.log('meu nome é ' + nome + ' e tenho ' + id + ' anos.')*/
+
+let nome = 'nitrato'
+ let cor = 'cinza'
+ console.log(`meu nome é ${nome} minha cor favorita é ${cor}`)
+
+ console.log(' meu nome é ' + nome + ' minha cor fav é ' + cor)
+
+ const nm = 'cleber'
+ console.log(nm.length)
+
+   const tt = 'DEIXA O LIKE'
+   const ttm = tt.toLowerCase()
+   console.log(ttm)
+
+const tv ='deixa o like'
+   const tvm = tv.toUpperCase()
+   console.log(tvm)
+
+
+   const email = '   junior.16@gmail.com    '//funçao trim
+console.log(email.trim())
+
+const frase = 'hoje eu comi pao'
+frase.includes('pao')
+frase.includes('comida')
+console.log(frase.includes('comida'))
+
+
+const ff = 'hj comi bolo , adoro bolo'//troca uma palavra por outra ou letra.
+
+const novaF = ff.replaceAll('bolo','feijao e arroz')
+
+console.log(novaF)
+
+
